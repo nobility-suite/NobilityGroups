86e12d848a8146450da58f3b66c5e1aacdbe11e0
KingVictoria <therealkingvictoria@gmail.com>
2019-12-28 21:39:46 -0500

    Initial Commit

---
+++ src/main/java/net/civex4/NobilityGroups.java
@@ -0,0 +1,22 @@
+package net.civex4;
+
+import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
+import vg.civcraft.mc.civmodcore.ACivMod;
+
+public class NobilityGroups extends ACivMod {
+    private static Logger logger;
+
+    private static NobilityGroups instance;
+
+    public void onEnable() {
+        instance = this;
+        logger = getLogger();
+
+        if (!Bukkit.getPluginManager().isPluginEnabled("NameLayer")){
+            logger.info("NobilityGroups is shutting down because it could not find NameLayer");
+            this.getPluginLoader().disablePlugin(this); // shut down
+        }
+    }
+}
